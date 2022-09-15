30c89e8c1ebfa641bd438238d3afb706156d60e5
Luigi Lupini <luigi.lupini@gmail.com>
2022-09-15 20:55:37 +0200

    12) <Link /> and <Route /> custom navigation setup

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
+// Step 1
 const Link = ({ className, href, children }) => {
+  const onClick = (event) => {
+    // Step 2
+    event.preventDefault();
+    window.history.pushState({}, "", href);
+    // Step 3
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent);
+  };
   return (
-    <a
-      onClick={(event) => event.preventDefault()}
-      className={className}
-      href={href}
-    >
+    <a onClick={onClick} className={className} href={href}>
       {children}
     </a>
   );
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
+
 const Route = ({ path, children }) => {
-  return window.location.pathname === path ? children : null;
+  // Step 5
+  const [urlPath, setUrlPath] = useState(window.location.pathname);
+  // Step 4
+  useEffect(() => {
+    const onLocationChange = () => {
+      setUrlPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", onLocationChange);
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []);
+  return urlPath === path ? children : null;
 };
 
 export default Route;
